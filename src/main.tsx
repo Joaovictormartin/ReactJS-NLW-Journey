7050c2ef0dde4befb431809e8603524c19d0ccd5
Joao Victor <devjoaovictor10@gmail.com>
2024-10-01 13:17:09 -0300

    configurando react-query e sonner

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import "./index.css";
 
 import React from "react";
+import { Toaster } from "sonner";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { App } from "./app";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster position="top-right" />
+    </QueryClientProvider>
   </React.StrictMode>
 );
